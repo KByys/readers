77df1fbd36848b5a2fdbbe4e3f976e7fbf351b1b
KByys <3138859984@qq.com>
2023-11-29 17:13:25 +0800

    test

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -181,3 +181,25 @@ impl<R: Read + Seek> Seek for StreamReaders<R> {
         }
     }
 }
+
+#[allow(unused)]
+mod test {
+
+    use super::*;
+    use std::io::BufReader;
+    #[test]
+    fn test() -> std::io::Result<()> {
+        std::fs::write("1", b"Hello,")?;
+        std::fs::write("2", b"Rust!")?;
+        let f1 = std::fs::File::open("1")?;
+        let f2 = std::fs::File::open("2")?;
+        let mut readers = StreamReaders::new();
+        readers.push(f1)?;
+        readers.push(f2)?;
+        let mut reader = BufReader::new(readers);
+        let mut buf = String::new();
+        reader.read_to_string(&mut buf)?;
+        assert_eq!("Hello,Rust!", buf.as_str());
+        Ok(())
+    }
+}
